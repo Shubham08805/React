c17a4508a7d052ed3940bfb3444c625ff57f0a61
Shubham <shubhamjnv11508805@gmail.com>
2022-11-12 20:20:47 +0530

    Changes in App.js

---
+++ daily-practice/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  let name='Shubham'
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>name</div>
+        <div>{name}</div>
+        <div>10+10</div>
+        <div>{10+10}</div>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
